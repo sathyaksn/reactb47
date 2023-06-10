1a4ce3c0faf728d1d576b594155d6c26fa6bc89c
sathyanarayanan <vijaysathi2001@gmail.com>
2023-06-10 21:41:47 +0530

    react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+// function App () {
+//java script
+// console.log('hello component')
+// const now =new Date();
+// const a =10;
+// const b =20;
+// console.log(now,a+b);
+
+
+//   return (
+//   <div>
+//     <p>hello world, it is {now.toString()}
+//     {a} plus{b} is{a+b}
+//     </p>
+   
+//   </div>
+//   );
+// }
+
+// export default App;
+ 
+import React from 'react'
+
+function Hello(props){
+return(
+  <div>
+<p>Hello {props.name} you are {props.age} old!</p>
+</div>
+)
+}  
+
 
 function App() {
+  const age=10;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Greatings</h1>
+      <Hello name='sathya' age={10+20}/>
+      <Hello name="narayanan" age={age}/>
+     
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
