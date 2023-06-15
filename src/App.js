e70f72aed5f3aea79d8f8a80ab63b595c5b26bec
sathyanarayanan <vijaysathi2001@gmail.com>
2023-06-15 20:19:42 +0530

    day5

--- src/App.js
+++ src/App.js
@@ -43,14 +43,140 @@
 //   }
 // }
 
-// function component using external folder
+// settimeout and usestate 
+// import React, { useState } from 'react'
+
+// function App() {
+//   const [counter,setcounter]=useState(0)
+// setTimeout(()=> 
+//   setcounter(counter+1),1000
+// );
+// console.log('hello',counter)
+//   return (
+//     <div>{counter}
+//     </div>
+//   )
+// // }
+
+//usestate method
+
+// import React, { useState } from 'react'
+// //function DIsplay(props)
+// const Display=(props)=>{
+//   return(
+// <div>{props.counter}</div>
+//   )
+//   }
+//   function Button(props){
+//     return(
+//       <button onClick={props.handleclick}>{props.text}</button>
+//     )
+//   }
+
+// function App() {
+//   const[counter,setcounter]=useState(0)
+//   const Handleplusclick=()=>{
+// setcounter(counter+1)
+//   }
+//   const Handleminusclick=()=>{
+//     setcounter(counter-1)
+//   }
+
+//       const Handlezeroclick=()=>{
+//         setcounter(0)
+//           }
+//   return (
+//     <div>
+//      <Display counter= {counter}/>
+
+// <Button text='plus' handleclick={Handleplusclick}/>
+// <Button text='minus' handleclick={Handleminusclick}/>
+// <Button text='zero' handleclick={Handlezeroclick}/>
+//     </div>
+//   )
+//   }
+// import React, { useState } from 'react'
+
+// function App() {
+//   const [isButtonEnabled,setButtonEnabled]=useState(false)
+  
+//   const handclick =()=>{
+   
+//     setButtonEnabled(true)
+    
+//   }
+  
+//   return (
+//     <div>
+//       <input/>
+//       <button onClick={handclick}>toggle</button>
+//       <button disabled={isButtonEnabled}>button</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import React, { useEffect, useState } from 'react'
+
+// function App() {
+//   const[count,setcount]=useState(0)
+
+//   useEffect (()=>{
+//     document.title=`count:${count}`;
+//   },[count]);
+
+//   const incrementhandler=()=>{
+//     setcount(count+1)
+//   }
+
+//   return (
+//     <div>
+//       <h1>document title update</h1>
+//       <button onClick={incrementhandler}>increment</button>
+//       </div>
+//   )
+// }
+
+// export default App
+
+//props drilling
+
 import React from 'react'
-import Hello from "./components/hello"
+
+function Child({parentdata}){
+  
+  return(
+  <div>
+    <h2>childu</h2>
+    <Grandfather parentdata={parentdata}/>
+  </div>
+ 
+  ) 
+}
+function Grandfather({parentdata}){
+  
+  return(
+  <div>
+    <h2>child</h2>
+    
+  </div>
+ 
+  )
+}
+
+
+
+
+
 function App() {
+
   return (
-    <div><Hello name={"sathya"} /></div>
+    <div>
+      <h1>parent coment</h1>
+<Child parentdata={'hello'} />
+    </div>
   )
 }
 
 export default App
-
--- src/index.js
+++ src/index.js
@@ -3,32 +3,32 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 // data
-const notes = [
-    {
-        id: 1,
-        content: 'Working with Lists',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Usage of keys',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'Conditional Rendering',
-        important: true
-    },
-    {
-        id: 4,
-        content: 'Handling Events',
-        important: true
-    },
-    {
-        id: 5,
-        content: 'Props Vs State',
-        important: false
-    }
-];
+// const notes = [
+//     {
+//         id: 1,
+//         content: 'Working with Lists',
+//         important: true
+//     },
+//     {
+//         id: 2,
+//         content: 'Usage of keys',
+//         important: false
+//     },
+//     {
+//         id: 3,
+//         content: 'Conditional Rendering',
+//         important: true
+//     },
+//     {
+//         id: 4,
+//         content: 'Handling Events',
+//         important: true
+//     },
+//     {
+//         id: 5,
+//         content: 'Props Vs State',
+//         important: false
+//     }
+// ];
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App notes={ notes } />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
